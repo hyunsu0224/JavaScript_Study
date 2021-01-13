e376e496e448f0f7bfdcb8f6fa03306a64decce4
hyunsu0224 <zsdry19203@gmail.com>
2021-01-13 18:10:41 +0900

    21-01-13 finish

--- jsFIle/weather.js
+++ jsFIle/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "2e470598bb557d8b5b943420aceb3d0d";
 const COORDS = "coords";
 
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+
 function handleGeoSucces(position) {
-    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
 }
 
 function handleGeoError() {
