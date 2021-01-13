b6187db458996d11674461a86d09dae383a7ea80
hyunsu0224 <zsdry19203@gmail.com>
2021-01-13 14:25:44 +0900

    21-01-13/afternoon

---
+++ jsFIle/weather.js
@@ -0,0 +1,28 @@
+const COORDS = "coords";
+
+function handleGeoSucces(position) {
+    console.log(position);
+}
+
+function handleGeoError() {
+    console.log("Cant access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null) {
+        askForCoords();
+    } else {
+        // getWeather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
